30bf562c40394fb8a47fd84832af6f6248301b7c
kanato-osajima <kanato.osajima@fox-hound.jp>
2025-04-23 15:37:13 +0900

    タスク2-3実装

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -3,19 +3,29 @@
 import { User } from "../types/User";
 import UserCard from "../components/UserCard";
 import { Box, Typography } from "@mui/material";
+import { useState } from "react";
 
 interface UserListProps {
   users: User[];
 }
 
 const UserList: React.FC<UserListProps> = ({ users }) => {
+  const [userList,setUserList] = useState<User[]>(users);
+
+  const handleDelete = (deletedUserId: number) => {
+    setUserList((initialUsers) =>
+      initialUsers.filter((user: User) => user.id !== deletedUserId)
+    );
+  };
+
   return (
     <Box>
       <Typography variant="h4" gutterBottom>
         ユーザー一覧
       </Typography>
-      {users.map((user) => (
-        <UserCard key={user.id} user={user} />
+
+      {userList.map((user) => (
+        <UserCard key={user.id} user={user} onDelete={handleDelete}/>
       ))}
     </Box>
   );
