76517e0ccf1ec9b304ed5adc4b5540421ac6f542
kanato-osajima <kanato.osajima@fox-hound.jp>
2025-04-23 16:24:09 +0900

    タスク3-1実装

---
+++ components/UserDetails.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {  Box, Typography, } from "@mui/material";
+import { fetchUserById, } from "../utils/api";
+import { User } from "@/types/User";
+
+interface UserDetailsProps {
+  userId: number;
+}
+
+const UserDetails: React.FC<UserDetailsProps> = ({
+  userId
+}) => {
+    const [user, setUser] = useState<User | null>(null); //型を定義
+
+
+  useEffect(() => {
+    const loadUser = async () => {
+        const fetchedUser = await fetchUserById(userId);
+        setUser(fetchedUser);
+      };
+  
+      loadUser();
+    }, [userId]);
+
+  return (
+    <Box sx={{ maxWidth: 400, margin: "auto", mt: 4 }}>
+    <Typography variant="h4" gutterBottom>
+      ユーザー詳細
+    </Typography>
+    <Typography variant="h6" gutterBottom>
+      ID: {user.id}
+    </Typography>
+    <Typography variant="h6" gutterBottom>
+      名前: {user.name}
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+      メールアドレス: {user.email}
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+      役割: {user.role}
+    </Typography>
+  </Box>
+
+
+  );
+};
+
+export default UserDetails;
