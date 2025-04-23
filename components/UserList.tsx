d1fe2a0e0ec38a4ea2f64dea72a60b2600961755
kanato-osajima <kanato.osajima@fox-hound.jp>
2025-04-23 11:50:28 +0900

    タスク1-1実装

---
+++ components/UserList.tsx
@@ -0,0 +1,26 @@
+
+"use client"; // クライアントコンポーネントとしてマーク
+
+import { User } from "../types/User";
+import UserCard from '../components/UserCard';
+import { Box, Typography } from "@mui/material";
+
+
+interface UserListProps {
+    users: User[];
+}
+
+const UserList: React.FC<UserListProps> = ({ users }) => {
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        ユーザー一覧
+      </Typography>
+      {users.map(user => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </Box>
+  );
+};
+
+export default UserList;
