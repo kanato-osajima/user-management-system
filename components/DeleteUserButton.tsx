3a3932aecc9e5e2666fb6299e8d9fc72c34e5d01
kanato-osajima <kanato.osajima@fox-hound.jp>
2025-04-23 14:37:59 +0900

    タスク2-2実装&DeleteUserButton修正

--- components/DeleteUserButton.tsx
+++ components/DeleteUserButton.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { Box, Button, Alert } from "@mui/material";
-import { fetchUserById, softDeleteUser } from "../utils/api";
+import { softDeleteUser } from "../utils/api";
 
 interface DeleteUserButtonProps {
   userId: number;
